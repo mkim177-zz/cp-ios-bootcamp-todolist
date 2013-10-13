ff2703670587ea4adf8a6f7c5d33b87e641b1eb5
Mark Kim <mkim@zynga.com>
2013-10-12 22:20:53 -0700

    Initial Commit

---
+++ TodoListRedo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TodoListRedo
+//
+//  Created by mkim on 10/12/13.
+//  Copyright (c) 2013 mkim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodoListRedo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TodoListRedo
+//
+//  Created by mkim on 10/12/13.
+//  Copyright (c) 2013 mkim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
